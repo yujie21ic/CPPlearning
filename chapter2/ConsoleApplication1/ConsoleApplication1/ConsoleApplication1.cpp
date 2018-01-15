10cc8d1e7fd14cd34eaea96e6a7ebfea966f5be5
yutiansut <yutiansut@qq.com>
2018-01-15 19:02:14 +0800

    #

---
+++ chapter2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int a;
+	std::cout << 1111 << std::endl;
+	std::cin >> a;
+	std::cout << a << std::endl;
+	
+
+    return 0;
+}
+
