d625809283400cde0f112c07577a0de01d92152f
yutiansut <yutiansut@qq.com>
2018-01-21 14:20:30 +0800

    #

--- chapter2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ chapter2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,16 +3,24 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+void func1();
 
 int main()
 {
 	int a;
 	std::cout << 1111 << std::endl;
+	
+	
+	func1();
 	std::cin >> a;
 	std::cout << a << std::endl;
-	
-
     return 0;
 }
 
+void func1()
+{
+	using namespace std;
+	for (int i = 0; i < 100;i++)
+		cout << "helloword!" << endl;
+	
+}
